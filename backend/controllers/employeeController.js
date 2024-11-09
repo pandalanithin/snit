9a31bcec7a14000c43ec875e7e8e666d609ebb83
pandalanithin <pandalanithin1512@gmail.com>
2024-11-09 10:24:25 +0530

    first commit

---
+++ backend/controllers/employeeController.js
@@ -0,0 +1,20 @@
+const Employee=require('../models/Employee')
+
+const createEmployee=async(req,res)=>{
+    try{
+        const {name,email,phone,city}=req.body
+        const employee=new Employee({
+            name,
+            email,
+            phone,
+            city
+        })
+        await employee.save();
+        res.status(201).json({message:"Employee data inserted successfully"})
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:'server error'})
+    }
+}
+
+module.exports={createEmployee};
